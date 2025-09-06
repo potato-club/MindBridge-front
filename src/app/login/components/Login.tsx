8819fd7c463c0f94a4fcb2476c52fc08048b8196
이현채 <hxonxthai@gmail.com>
2025-09-06 17:02:43 +0900

    css 수정중. 어떤 기능을 넣어야 하나...

--- src/app/login/components/Login.tsx
+++ src/app/login/components/Login.tsx
@@ -1,87 +1,77 @@
 'use client';
 
-import { useEffect } from "React";
+import { useEffect, useState} from "react";
 import styles from "./Login.module.css";
 import { useRouter } from "next/navigation";
 
 
 
-
 const LoginForm = () => {
+    const [userId, setUserId] = useState("");
+    const [userPassword, setUserPassword] = useState("");
+
     const router = useRouter();
-}
 
-export default function LoginPage() {
+
     return (
-        <div className="Container">
-            <div className="Login-Page">
-                <h1>로그인 페이지</h1>
-                <form>
-                    <LogoAndAppName />
-                    <UserIdInput />
-                    <PasswordInput />
-                    <LoginButton />
-                </form>
-                <FindIdOrPassword />
-                <SignUp />
-            </div>
-        </div>
+        <>
+            <header className={styles.header}>로그인</header>
+            <form>
+                <div className={styles.Container}>
+
+                    {/* 어플명 or 로고 + 설명 */}
+                    <div className={styles.LogoAndAppName}>
+                        <h2 className={styles.AppName}>MindBridge</h2>
+                        <p className={styles.AppDescription}>당신의 마음을 이어주는 다리</p>
+                    </div>
+
+                    {/* 아이디 */}
+                    <div className={styles.UserId}>
+                        <input type="text"
+                            id="userId"
+                            name="userId"
+                            placeholder="아이디를 입력해주세요." 
+                        />
+                    </div>
+
+                    {/* 비밀번호 */}
+                    <div className={styles.UserPassword}>
+                        <input type="password"
+                            id="userPassword"
+                            name="userPassword"
+                            placeholder="비밀번호 입력해주세요." 
+                        />
+                    </div> 
+
+                    
+
+                    {/* 로그인 버튼 */}
+                    <div className={styles.LoginButton}>
+                        <button type="submit">
+                            <p className={styles.LoginButtonText}>로그인</p>
+                        </button>
+                    </div> 
+
+                    {/* 하단 영역 */}
+                    <div className={styles.LoginFooter}>
+                        {/* 아이디/비밀번호 찾기 */}
+                        <div className={styles.FindIdAndPassword}>
+                            아이디/비밀번호 찾기
+                        </div>
+
+                        {/* 회원가입 */}
+                        <div className={styles.SignUp}>
+                            회원가입
+                        </div>
+                    </div>
+
+                    
+
+                </div>
+            </form>
+        </>
     );
-}
 
-/* 로고 + 앱 이름 컴포넌트 */
-const LogoAndAppName = () => {
-    return (
-        <div className="LogoAndAppName">
-            <h2 className="App-Name">MindBridge</h2>
-        </div>
-    );
-}
+};
 
-/* 아이디 */
-const UserIdInput = () => {
-    return (
-        <div className="User-ID">
-            <label htmlFor='user-id'>아이디</label>
-            <input type="text" id="user-id" name="user-id" placeholder="아이디를 입력하세요" />
-        </div>
-    )
-}
-
-/* 비밀번호 */
-const PasswordInput = () => {
-    return (
-        <div className="Password">
-            <label htmlFor='password'>비밀번호</label>
-            <input type="password" id="password" name="password" placeholder="비밀번호를 입력하세요" />
-        </div>
-    )
-}   
-
-/* 로그인 버튼 */
-const LoginButton = () => {
-    return (
-        <div className="Login-Button">
-            <button type="submit">로그인</button>
-        </div>
-    )
-}   
-
-/* 비밀번호/아이디 찾기 */
-const FindIdOrPassword = () => {
-    return (
-        <div className="Find-id-Password">
-            <a href="/find-id">아이디 찾기</a>
-            <a href="/find-password">비밀번호 찾기</a>
-        </div>
-    )
-}
-
-/* 회원가입 */
-const SignUp = () => {
-    return (
-        <div className="Sign-Up">
-            <a href="/sign-up">회원가입</a>
-        </div>
-    )
-}   
+export default LoginForm;
