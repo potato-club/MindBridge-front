82691025bdab94e412f93af9019e3330f689b750
이현채 <hxonxthai@gmail.com>
2025-09-07 01:30:50 +0900

    기능 다시 정리하고, API구현해야하는데,,,

--- src/app/login/components/Login.tsx
+++ src/app/login/components/Login.tsx
@@ -1,17 +1,66 @@
 'use client';
 
-import { useEffect, useState} from "react";
-import styles from "./Login.module.css";
+import { useState} from "react";
 import { useRouter } from "next/navigation";
+import styles from "./Login.module.css";
 
 
+/* API 음,,, 백엔드랑 연동해야함 
+import NextAuth from "next-auth/next";
+import CredentialsProvider from "next-auth/providers/credentials";
 
+const handler = NextAuth({
+    providers: [
+        CredentialsProvider({
+            name: "credentials",
+
+            /* 로그인 폼에서 입력한 값을 받아옴 
+            credentials: {
+                userId: { label: "userId", type: "text" },
+                userPassword: { label: "userPassword", type: "password" },
+            },  
+
+            /* 로그인 처리 
+            async authorize(credentials, req) {
+                /* DB 연동 음,,,,, 왓 이즈 백엔드API요청,,,ㅜㅜ
+
+                if (user) {
+                    return user;
+                } else {
+                    return null;
+                }
+            },
+        }),
+    ],
+});    
+
+export {handle as GET, handler as POST};
+*/
+
+
+/* 로그인 폼 */
 const LoginForm = () => {
     const [userId, setUserId] = useState("");
     const [userPassword, setUserPassword] = useState("");
 
     const router = useRouter();
 
+    const handleCudmit = async (e) => {
+        e.prevenDefault();
+
+        const result = await signIn("credentials", {
+            redirect: false,
+            userId, 
+            userPassword,
+        });
+
+        if (result.ok) {
+            router.push("/메인페이지"); /* 메인페이지 경로로 변경해야한다ㅡ,,, */
+        } else {
+            alert("로그인 실패");
+            router.push("/Login");
+        }
+    };
 
     return (
         <>
@@ -28,8 +77,8 @@ const LoginForm = () => {
                     {/* 아이디 */}
                     <div className={styles.UserId}>
                         <input type="text"
-                            id="userId"
-                            name="userId"
+                            value={userId}
+                            onChange={(e) => setUserId(e.target.value)}
                             placeholder="아이디를 입력해주세요." 
                         />
                     </div>
@@ -37,8 +86,8 @@ const LoginForm = () => {
                     {/* 비밀번호 */}
                     <div className={styles.UserPassword}>
                         <input type="password"
-                            id="userPassword"
-                            name="userPassword"
+                            value={userPassword}
+                            onChange={(e) => setUserPassword(e.target.value)}
                             placeholder="비밀번호 입력해주세요." 
                         />
                     </div> 
