bcf1f352d62cc1ec4f81f2b34ca7e5db2e1160a5
이현채 <hxonxthai@gmail.com>
2025-12-31 14:31:21 +0900

    마일리지 모달다는 중

--- src/app/(mypage)/mypage/MyPage.tsx
+++ src/app/(mypage)/mypage/MyPage.tsx
@@ -11,6 +11,18 @@ import MyBottomForm from "@/app/(mypage)/mybottom/MyBottom";
 const MyPageForm = () => {
     const router = useRouter();
 
+    const [open, setOpen] = useState(false);
+    const handleWithDraw = async () => {
+        alert("마일리지를 획득해 보세요!");
+        setOpen(false);
+        router.push('/mypage');
+    };
+
+    const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+        setOpen(true);
+    };
+
     return (
         <>
             <header className={styles.header}>마이페이지</header>
@@ -94,6 +106,45 @@ const MyPageForm = () => {
                     </div>
                 </div>
             </form>
+
+
+                        {/* 🔸 모달 (open === true일 때만 표시) */}
+            {open && (
+                <div className={styles.overlay}>
+                    <div className={styles.modal}>
+                        <h2>마일리지 획득 방법</h2>
+
+                        <h4>1. 채팅 고민 해결 참여</h4>
+                        <p>고민을 나누고 해결에 참여해보세요</p>
+
+                        <h4>2. 게시글, 댓글 좋아요 달성</h4>
+                        <p>작성한 게시글, 댓글의 일정 수 이상의 좋아요를 받아보세요</p>
+
+                        <h4>3. 주간 인기 게시글 선정</h4>
+                        <p>한 주 동안 가장 많은 관심을 받은 게시글로 선정되어 보세요</p>
+
+                        <h4>4. 하루 한 번 랜덤 돌리기</h4>
+                        <p>---------</p>
+
+                        <div className={styles.buttons}>
+                            <button
+                                type="button"
+                                className={styles.cancelBtn}
+                                onClick={() => setOpen(false)}
+                            >
+                                닫기
+                            </button>
+                            <button
+                                type="button"
+                                className={styles.confirmBtn}
+                                onClick={handleWithDraw}
+                            >
+                                확인
+                            </button>
+                        </div>
+                    </div>
+                </div>
+            )}
         </>
 
     );
